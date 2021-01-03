b7ffeb567a2972fc35e03965506567c2a96b7698
suresh vempalli <sureshvitry@example.com>
2021-01-03 16:04:44 +0530

    v 7

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const posts = require('./routes/api/posts');
 app.use('/api/posts',posts);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const port =  4000;
 const server = app.listen(port, function () {
     console.log('Server listening on port ' + port);
 });
